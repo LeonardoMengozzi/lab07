dae4a8c72970414ca89a9ed62f6b905aadd7c63b
Leonardo Mengozzi <leonardo.mengozzi2@studio.unibo.it>
2025-11-05 17:04:19 +0100

    72 done at home

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -4,7 +4,7 @@ import it.unibo.functional.api.Function;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.List; 
 import java.util.Objects;
 
 /**
@@ -54,7 +54,14 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<? extends O>>() {
+
+            @Override
+            public Collection<? extends O> call(I input) {
+                return List.of(transformer.call(input));
+            }
+            
+        });
     }
 
     /**
@@ -70,7 +77,7 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
@@ -87,7 +94,18 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<I>>() {
+
+            @Override
+            public Collection<I> call(I input) {
+                var l = new ArrayList<I>();
+                if (test.call(input)) {
+                    l.add(input);
+                }
+                return l;
+            }
+            
+        });
     }
 
     /**
@@ -103,6 +121,13 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return select(base, new Function<I, Boolean>() {
+
+            @Override
+            public Boolean call(I input) {
+                return !test.call(input);
+            }
+            
+        });
     }
 }
--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -3,19 +3,68 @@ package it.unibo.nestedenum;
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    private enum Month {
+        JANUARY(31), 
+        FEBRUARY(28), 
+        MARCH(31), 
+        APRIL(30), 
+        MAY(31), 
+        JUNE(30), 
+        JULY(31), 
+        AUGUST(31),
+        SEPTEMBER(30), 
+        OCTOBER(31), 
+        NOVEMBER(30), 
+        DECEMBER(31);
+
+        private final int days;
+
+        Month(final int days) {
+            this.days = days;
+        }
+
+        public static Month fromString(final String monthName) {
+            final String formatted = monthName.toUpperCase();
+            Month res = null;
+            int monthFind = 0;
+            for (final var month : Month.values()) {
+                if (month.name().startsWith(formatted)) {
+                    res = month;
+                    monthFind++;
+                }
+            }
+            if (monthFind == 1) {
+                return res;
+            }
+            throw new IllegalArgumentException();
+        }
+
+    }
+
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(String arg0, String arg1) {
+                return Integer.compare(Month.fromString(arg0).days, Month.fromString(arg1).days);
+            }
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(String arg0, String arg1) {
+                return Month.fromString(arg0).compareTo(Month.fromString(arg1));
+            }
+        };
     }
 }
